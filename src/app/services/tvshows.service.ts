d0f2715224aa2bf8d6712730334ffc0a5f5f7d4b
Christopher Curley <clevercurley@gmail.com>
2024-02-18 17:59:13 -0500

    implement tvshows service

---
+++ src/app/services/tvshows.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { MoviesDto } from '../types/movie';
+import { map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TvshowsService {
+  private apiUrl = 'https://api.themoviedb.org/3/';
+  private TMDB_API_KEY = '37afcf28b5e4f6f793672f8258fcfea5';
+
+  constructor(private http: HttpClient) {}
+
+  getMoviesByType(type: string, count = 20) {
+    return this.http
+      .get<MoviesDto>(`${this.apiUrl}tv/${type}?api_key=${this.TMDB_API_KEY}`)
+      .pipe(map((data) => data.results.slice(0, count)));
+  }
+}
